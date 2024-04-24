9f3f0c7e9166acf70a6098cd0087180f4ae811e1
saraxplorer <147872964+saraxplorer@users.noreply.github.com>
2024-04-24 18:16:29 +0200

    Update ft_strcpy.c

--- Level 1/ft_strcpy/ft_strcpy.c
+++ Level 1/ft_strcpy/ft_strcpy.c
@@ -1,1 +1,22 @@
 
+char    *ft_strcpy(char *s1, char *s2)
+{
+	int i = 0;
+
+	while(s2[i]!='\0')
+	{
+		s1[i] = s2[i];
+		i++;
+	}
+	s1[i] = '\0';//necessary
+	return(s1);
+}
+
+#include <stdio.h>
+int main()
+{
+	char s1[10];//segfaults if used pointer
+	char *s2 = "chf ufb";
+	ft_strcpy(s1, s2);
+	printf("%s", s1);
+}
