830abbc65fa8c2a4002f589b4c0334b40a125b6f
saraxplorer <147872964+saraxplorer@users.noreply.github.com>
2024-05-08 11:06:13 +0200

    Update first_word.c

--- Level 1/first_word/first_word.c
+++ Level 1/first_word/first_word.c
@@ -1,14 +1,6 @@
 #include<unistd.h>
 #include<stdio.h>
 
-int ft_strlen(char *str)
-{
-    int i;
-    i = 0;
-    while(str[i]!= '\0')
-        i++;
-    return(i);
-}
 void display(char *str)
 {
     int i = 0;
