70e52ab19191259a238a3f9c87bf33cd41eb265a
saraxplorer <147872964+saraxplorer@users.noreply.github.com>
2024-05-21 17:59:06 +0200

    Add files via upload

--- Level 4/atoi_base/ft_atoi_base.c
+++ Level 4/atoi_base/ft_atoi_base.c
@@ -1,43 +1,37 @@
-int char_to_int(char c, int base)
+int ft_atoi_base(char *str, int base)
 {
-  int max_digit;
-  if (base <= 10)//decimal or smaller
-    max_digit = base + '0';//turn base into int
-  else//bigger than 10, means hexadecimal
-    max_digit = base - 10 + 'a'; //16- 10 + 'a == 102, ascii of f//hexadecimal base
-  if (c >= '0' && c <= '9' && c <= max_digit)//numerical char
-		return (c - '0');//numeric int/ascii
-	else if (c >= 'a' && c <= 'f' && c <= max_digit)//lowercase hexa
-		return (10 + c - 'a');//ascii 
-	else
-		return (-1);
+	int i = 0;
+	int result = 0;
+	int value = 0;//to process chars
+	int sign = 1;
+	if (str[0] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	while (str[i] != '\0')
+	{
+		if ((str[i]>= '0' && str[i] <= '9'))
+			value = str[i] - '0';//add the prev results
+		
+		else if (str[i]>= 'a' && str[i]<= 'f')
+			value = str[i] - 'a' + 10;
+		
+		else if (str[i]>= 'A' && str[i]<= 'F')
+			value = str[i] - 'A' + 10;
+		else
+			return 0;//for invalid cases
+		if (value >= base)// Ensure the value is valid for the given base
+			return 0;
+		result = result * base + value;
+		i++;
+	}
+	return (sign * result);
 }
-int ft_atoi_base(const char *str, int str_base)
-{
-  int result = 0;
-  int sign = 1;
-  int digit;
-  int i = 0;
-  char c;
 
-  while (str[i] != '\0')
-  {
-    
-    if (str[0] == '-')//handle minus sign
-    {
-       sign = -1;
-       i++;
-    }
-    if (str[i] >= 'A' && str[i] <= 'Z') //Handle uppercase
-        c = c + ('a' - 'A');//make all lowercase, and store it in a char, because we 
-                          //cannot modify a const str
-    digit = char_to_int(c, str_base);
-    while (digit >= 0)
-    {
-      result = result * str_base;//move one digit
-      result = result + (digit *sign);//??
-      i++;//is it right?
-    }
-    i++;
-  }
+#include <stdio.h>
+int main()
+{
+	char *input = "123";
+	printf("%d", ft_atoi_base(input, 20));//return of the function printed
 }
