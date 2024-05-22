f05d2d94f6f752c485b385e527038face8b4b923
saraxplorer <147872964+saraxplorer@users.noreply.github.com>
2024-05-22 15:29:08 +0200

    Add files via upload

--- Level 4/atoi_base/ft_atoi_base.c
+++ Level 4/atoi_base/ft_atoi_base.c
@@ -1,37 +1,29 @@
-int ft_atoi_base(char *str, int base)
+int	ft_atoi_base(const char *str, int str_base)
 {
-	int i = 0;
-	int result = 0;
-	int value = 0;//to process chars
-	int sign = 1;
-	if (str[0] == '-')
-	{
-		sign = -1;
-		i++;
-	}
-	while (str[i] != '\0')
-	{
-		if ((str[i]>= '0' && str[i] <= '9'))
-			value = str[i] - '0';//add the prev results
-		
-		else if (str[i]>= 'a' && str[i]<= 'f')
-			value = str[i] - 'a' + 10;
-		
-		else if (str[i]>= 'A' && str[i]<= 'F')
-			value = str[i] - 'A' + 10;
-		else
-			return 0;//for invalid cases
-		if (value >= base)// Ensure the value is valid for the given base
-			return 0;
-		result = result * base + value;
-		i++;
-	}
-	return (sign * result);
-}
+    int result = 0;
+    int i = 0;
+    int sign = 1;
+    if (str[0] == '-')
+        sign = -1;
+    while(str[i])
+    {
+        if (str[i]>= '0' && str[i] <= '9')
+            result = result * str_base + (str[i] - '0');
+    
+        else if (str[i]>= 'a' && str[i] <= 'f')
+            result = result * str_base + (str[i] - 'a' + 10) ;
+        
+        else if (str[i]>= 'A' && str[i] <= 'F')
+            result = result * str_base + (str[i] - 'A' + 10);
 
-#include <stdio.h>
+        i++;
+    }
+    return (sign * result);
+}
+#include<stdio.h>
 int main()
 {
-	char *input = "123";
-	printf("%d", ft_atoi_base(input, 20));//return of the function printed
+    char *s = "b";
+    printf("%d", ft_atoi_base(s, 16));
+
 }
